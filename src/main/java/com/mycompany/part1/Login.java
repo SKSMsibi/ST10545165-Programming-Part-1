64a94c4642b72f6a72209d035f412a1599c6823c
Sandile Msibi <st10545165@rcconnect.edu.za>
2026-09-18 21:51:44 +0100

    Added login class

---
+++ src/main/java/com/mycompany/part1/Login.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.part1;
+
+/**
+ *
+ * @author Sandile Msibi ROG
+ */
+public class Login {
+    public boolean checkUserName() 
+    {
+    boolean result = false;
+        return result;
+    }
+    
+    public boolean checkPasswordComplexity() 
+    {
+    boolean result = false;
+        return result;
+    }
+    
+    public boolean checkCellPhoneNumber()
+    {
+    boolean result = false;
+        return result;
+    }
+    
+    public String registerUser () 
+    
+    { 
+        String registrationMessage;
+        registrationMessage = "Sandile";
+        return registrationMessage;
+    }
+    
+    public boolean loginUser()
+    
+    {
+    boolean result = false;
+        return result;
+    }
+    
+    public String returnLoginStatus()
+    {
+        String loginStatus;
+        loginStatus = "Sandile";
+        return loginStatus;
+    }
+    
+    
+}
--- src/main/java/com/mycompany/part1/Part1.java
+++ src/main/java/com/mycompany/part1/Part1.java
@@ -11,6 +11,6 @@ package com.mycompany.part1;
 public class Part1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome Please Enter Your User Name: ");
     }
 }
