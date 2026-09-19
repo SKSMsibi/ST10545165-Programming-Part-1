59c842311a1c0da07d8e211d33cb529bd5882630
Sandile Msibi <st10545165@rcconnect.edu.za>
2026-09-19 17:55:07 +0100

    Added user Inputs

--- src/main/java/com/mycompany/part1/Login.java
+++ src/main/java/com/mycompany/part1/Login.java
@@ -9,22 +9,67 @@ package com.mycompany.part1;
  * @author Sandile Msibi ROG
  */
 public class Login {
-    public boolean checkUserName() 
+    public boolean checkUserName(String userName) 
     {
-    boolean result = false;
-        return result;
+        //Check is the user name contain an under score and not more than five characters
+    if (userName == null) {
+        return false;
+            
+        }
+
+        boolean containsUnderscore = userName.contains("_");
+        boolean lengthValid = userName.length() <= 5;
+
+        return containsUnderscore && lengthValid;
     }
     
-    public boolean checkPasswordComplexity() 
+    public boolean checkPasswordComplexity(String userPassword) 
     {
-    boolean result = false;
-        return result;
+    // This method checks is the password meets the required compleexity rules.
+        //The password must be: • At least eight characters long. • Contain a capital letter. • Contain a number. • Contain a special character.
+       if (userPassword == null) {
+        return false;
+    }
+
+    boolean hasMinLength = userPassword.length() >= 8;
+    boolean hasCapital = false;
+    boolean hasNumber = false;
+    boolean hasSpecial = false;
+
+    for (char c : userPassword.toCharArray()) {
+        if (Character.isUpperCase(c)) {
+            hasCapital = true;
+        } else if (Character.isDigit(c)) {
+            hasNumber = true;
+        } else if (!Character.isLetterOrDigit(c)) {
+            hasSpecial = true;
+        }
+    }
+
+    return hasMinLength && hasCapital && hasNumber && hasSpecial;
     }
     
-    public boolean checkCellPhoneNumber()
+    public boolean checkCellPhoneNumber(String userCellphone)
     {
-    boolean result = false;
-        return result;
+        //This method ensures that the cell phone is the correct length and contains the international country code.
+   if (userCellphone == null) {
+        return false;
+    }
+
+    boolean hasCountryCode = userCellphone.startsWith("+27");
+    boolean hasCorrectLength = userCellphone.length() == 12;
+
+    // Check if every character after the '+' is a number. 
+    boolean digitsOnly = true;
+    for (int i = 1; i < userCellphone.length(); i++) {
+        char c = userCellphone.charAt(i);
+        if (!Character.isDigit(c)) {
+            digitsOnly = false;
+            break;
+        }
+    }
+
+    return hasCountryCode && hasCorrectLength && digitsOnly;
     }
     
     public String registerUser () 
@@ -33,6 +78,8 @@ public class Login {
         String registrationMessage;
         registrationMessage = "Sandile";
         return registrationMessage;
+        
+        
     }
     
     public boolean loginUser()
--- src/main/java/com/mycompany/part1/Part1.java
+++ src/main/java/com/mycompany/part1/Part1.java
@@ -13,23 +13,27 @@ public class Part1 extends Login {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        Login newUser = new Login();
+        Login newUserObj = new Login();
         
         System.out.println("Welcome Please Enter Your User Name: ");
         String userName = input.nextLine();
         
         // Check the user name
-        
+        newUserObj.checkUserName(userName);
         
         System.out.println("Please Enter Your Password: ");
         String userPassword = input.nextLine();
         
         //Check Password
+        newUserObj.checkPasswordComplexity(userPassword);
+        
         
         System.out.println("Please Enter Your Callphone: ");
         String userCellphone = input.nextLine();
         
         //Check Phone number
+        newUserObj.checkCellPhoneNumber(userCellphone);
+        
         
         //Register User
         
