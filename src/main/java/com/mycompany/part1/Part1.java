64df957453056e7c3c2ed8bdec4e488ae1d8f60c
Sandile Msibi <st10545165@rcconnect.edu.za>
2026-09-19 16:45:10 +0100

    Added user Inputs

--- src/main/java/com/mycompany/part1/Part1.java
+++ src/main/java/com/mycompany/part1/Part1.java
@@ -3,14 +3,42 @@
  */
 
 package com.mycompany.part1;
-
+import java.util.Scanner;
 /**
  *
  * @author Sandile Msibi ROG
  */
-public class Part1 {
+public class Part1 extends Login {
 
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        Login newUser = new Login();
+        
         System.out.println("Welcome Please Enter Your User Name: ");
+        String userName = input.nextLine();
+        
+        // Check the user name
+        
+        
+        System.out.println("Please Enter Your Password: ");
+        String userPassword = input.nextLine();
+        
+        //Check Password
+        
+        System.out.println("Please Enter Your Callphone: ");
+        String userCellphone = input.nextLine();
+        
+        //Check Phone number
+        
+        //Register User
+        
+        //Login User
+        
+        System.out.println(userName);
+        System.out.println(userPassword);
+        System.out.println(userCellphone); 
+        
     }
+    
 }
